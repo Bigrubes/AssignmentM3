dc64458c3200df362645c4e25d6e68ecb7752c83
Reuben Ramos <rsramos23@gmail.com>
2023-10-02 14:02:03 -0700

    finished larger and smaller

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,15 @@
+
+function large()  {
+let num1 = parseInt(prompt('Please input first number'))
+let num2 = parseInt(prompt('Please input second number'))
+
+if (num1 > num2){
+alert(`${num1} is the bigger number`)
+}
+else if(num2 > num1){
+alert(`${num2} is the bigger number`)
+}
+else if(num1 === num2)
+alert('Both numbers are the same')
+}
+
