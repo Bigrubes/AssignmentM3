32b0c266ab3f105f9ba5b7e7d4678f0a321bc826
Reuben Ramos <rsramos23@gmail.com>
2023-10-03 16:14:20 -0700

    fixed larger smaller code

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -9,7 +9,7 @@ alert(`${num1} is the bigger number`)
 else if(num2 > num1){
 alert(`${num2} is the bigger number`)
 }
-else if(num1 === num2)
+else {(num1 === num2)
 alert('Both numbers are the same')
 }
-
+}
