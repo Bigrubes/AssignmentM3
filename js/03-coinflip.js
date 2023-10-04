95e736c877e3e98c7a3e2eb10897e43d48317121
Reuben Ramos <rsramos23@gmail.com>
2023-10-04 12:49:00 -0700

    fixed nested statements in coinflip

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -8,23 +8,34 @@ let tails = "tails"
 
 let coinFlip = Math.round(Math.random()* 100)
 console.log(coinFlip)
-if (coinFlip < 50)
+if (coinFlip < 50){
     coinFlip = heads
+}
     console.log(coinFlip)
-    if (coinFlip >= 50){
-        coinFlip = tails
-        console.log(coinFlip)
-    }
-if (coinFlip === tails && choice=== tails){ 
+if (coinFlip >= 50){
+    coinFlip = tails
+}
+console.log(coinFlip)
+
+
+if (coinFlip === tails){ 
+    if(choice=== tails){
     alert("The flip was tails and you chose tails...you win!")
+    }
 }
- else if (coinFlip === tails && choice === heads) {
+if (coinFlip === tails) {
+    if (choice === heads){
     alert("The flip was tails and you chose heads...you lose!")
+    }
 }
- else if (coinFlip  === heads && choice === heads) {
+if (coinFlip  === heads ){
+    if (choice === heads){
     alert("The flip was heads and you chose heads...you win!")
+    }
 }
- else {(coinFlip === heads && choice === tails) 
+if (coinFlip === heads){
+    if (choice === tails){
     alert("The flip was heads and you chose tails...you lose!")
+}
 }
 }
